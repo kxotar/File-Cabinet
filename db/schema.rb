0662deff669fdb580b3c96cdd4aed00c9ff1fcd4
shhhRakesh <sharma26.riti@gmail.com>
2017-03-12 01:31:21 +0530

    Doc Model Added

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema.define(version: 20170311195737) do
+
+  create_table "docs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
